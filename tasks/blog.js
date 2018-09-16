4d0b53aacf1efddeda4823b7fa247ec3d4c3153a
Alexander Shutov <shutovby@gmail.com>
2018-09-16 23:34:54 +0300

    Improved OG meta tags

--- tasks/blog.js
+++ tasks/blog.js
@@ -16,15 +16,30 @@ async function writePosts() {
     async function writePost({id, date, headline}) {
         const markdown = await fs.readFile(`src/blog/posts/${id}.md`, {encoding: 'utf8'});
         const contentHTML = marked(markdown);
+
         const imgMatch = contentHTML.match(/<img[\s\S]+?src="(.+?)"/);
-        let previewURL = imgMatch ? imgMatch[1] : '/images/darkreader-dynamic-mode-twitch.png';
-        if (!previewURL.startsWith('http')) {
+        let previewURL = imgMatch ? imgMatch[1] : '/images/darkreader-preview-medium.png';
+        if (!previewURL.startsWith('https://')) {
             previewURL = `https://darkreader.org/${previewURL.replace(/^\//, '')}`;
         }
+
+        const pMatch = contentHTML.match(/<p.*?>([\s\S]+?)<\/p>/);
+        let description = pMatch
+            ? pMatch[1]
+                .replace(/<script.*?>.*?<\/script>/g, '')
+                .replace(/<.*?>/g, '')
+                .replace(/\s+/g, ' ')
+            : 'Dark Reader news and helpful articles';
+        const firstPeriod = description.indexOf('.');
+        const secondPeriod = firstPeriod > 0 ? description.indexOf('.', firstPeriod + 1) : -1;
+        description = description.substring(0, (secondPeriod > 0 ? secondPeriod : firstPeriod) + 1);
+
         let result = template;
+        result = replace(result, '$ID', id);
         result = replace(result, '$HEADLINE', headline);
         result = replace(result, '$DATE', formatDate(date));
         result = replace(result, '$PREVIEW', previewURL);
+        result = replace(result, '$DESCRIPTION', description);
         result = replace(result, '$CONTENT', contentHTML);
         await fs.outputFile(`www/blog/${id}/index.html`, result, {encoding: 'utf8'});
     }
