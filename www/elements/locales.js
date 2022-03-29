d0215279c543ef08ff3a26737527147bb9de1223
Alexander Shutau <shutovby@gmail.com>
2022-03-29 16:11:35 +0100

    Update sponsors

--- www/elements/locales.js
+++ www/elements/locales.js
@@ -556,5 +556,8 @@ export const country = (() => {
 const hCountries = ['US', 'GB', 'CA', 'AU', 'DE', 'NL', 'FR'];
 export const isHCountry = hCountries.includes(country);
 
+/*
 const aliCountries = ['BR', 'CL', 'AD', 'AT', 'BE', 'BA', 'BG', 'CH', 'CY', 'CZ', 'DE', 'DK', 'EE', 'ES', 'FO', 'FI', 'FR', 'GR', 'HU', 'HR', 'IE', 'IS', 'IT', 'LT', 'LU', 'LV', 'MC', 'MK', 'MT', 'NO', 'NL', 'PO', 'PT', 'RO', 'SE', 'SI', 'SK', 'SM', 'TR'];
 export const isAliCountry = aliCountries.includes(country);
+*/
+export const isAliCountry = false;
